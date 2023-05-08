11b991dfafc71f153508fbdf3b9f7f9201c25fe6
katakuri-it <kutmanov_b@iuca.kg>
2023-05-09 00:00:22 +0300

    controller 1.0

---
+++ library/Controllers/ClientController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Library.Domain.DataTransferObject;
+using Library.BLL.Interfaces;
+using Library.BLL.interfaces;
+
+namespace library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientServices _clientServices
+    }
+}
