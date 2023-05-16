cae5a5ceb257a99db986b043e18bef30c336fec0
katakuri-it <kutmanov_b@iuca.kg>
2023-05-16 22:20:30 +0300

    service final

--- Library.BLL/Services/EmployeeServices.cs
+++ Library.BLL/Services/EmployeeServices.cs
@@ -1,34 +1,124 @@
 ﻿using Library.BLL.interfaces;
+using Library.DLL.Interfaces;
 using Library.Domain.DataTransferObject;
 using Library.Domain.Entities;
+using System.Numerics;
 
 namespace Library.BLL.Services
 {
     public class EmployeeServices : IEmployeeServices
     {
-        public Task<EmployeeDto> CreateAsync(EmployeeDto entity)
+        private readonly IEmployeeRepository _repository;
+        private readonly IOrderRepository _orderRepository;
+        public EmployeeServices(IEmployeeRepository repository, IOrderRepository orderRepository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+            _orderRepository = orderRepository;
+        }
+        public async Task<EmployeeDto> CreateAsync(EmployeeDto entity)
+        {
+            var employee = new Employee
+            {
+                Id = entity.Id,
+                FullName = entity.FullName,
+                BirthDate = entity.BirthDate,
+                Position = entity.Position,
+                Salary = entity.Salary,
+                AddressId = entity.AddressId,
+                Phone = entity.Phone,
+            };
+            var result = await _repository.CreateAsync(employee);
+            return new EmployeeDto
+            {
+                Id = result.Id,
+                FullName = result.FullName,
+                BirthDate = result.BirthDate,
+                Position = result.Position,
+                Salary = result.Salary,
+                AddressId = result.AddressId,
+                Phone = result.Phone
+            };
         }
 
-        public Task<EmployeeDto> DeleteAsync(int id)
+        public async Task<EmployeeDto> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _repository.DeleteAsync(id);
+            return new EmployeeDto
+            {
+                Id = result.Id,
+                FullName = result.FullName,
+                BirthDate = result.BirthDate,
+                Position = result.Position,
+                Salary = result.Salary,
+                AddressId = result.AddressId,
+                Phone = result.Phone,
+            };
         }
 
-        public Task<IEnumerable<EmployeeDto>> GetAsync()
+        public async Task<IEnumerable<EmployeeDto>> GetAsync()
         {
-            throw new NotImplementedException();
+            var results = await _repository.GetAsync();
+            return (from result in results
+                    select
+                   new EmployeeDto
+                   {
+                       Id = result.Id,
+                       FullName = result.FullName,
+                       BirthDate = result.BirthDate,
+                       Position = result.Position,
+                       Salary = result.Salary,
+                       AddressId = result.AddressId,
+                       Phone = result.Phone,
+                   }).ToList();
         }
 
-        public Task<EmployeeDto> GetAsync(int id)
+        public async Task<EmployeeDto> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _repository.GetAsync(id);
+            return new EmployeeDto
+            {
+                Id = result.Id,
+                FullName = result.FullName,
+                BirthDate = result.BirthDate,
+                Position = result.Position,
+                Salary = result.Salary,
+                AddressId = result.AddressId,
+                Phone = result.Phone,
+                Order = (from results in result.Order
+                         select
+                         new OrderDto
+                         {
+                             Id = results.Id,
+                             DateOfIssue = results.DateOfIssue,
+                             ClientId = results.ClientId,
+                             EmployeeId = results.EmployeeId,
+                         }).ToList(),
+            };
         }
 
-        public Task<EmployeeDto> UpdateAsync(EmployeeDto entity)
+        public async Task<EmployeeDto> UpdateAsync(EmployeeDto entity)
         {
-            throw new NotImplementedException();
+            var employee = new Employee
+            {
+                Id = entity.Id,
+                FullName = entity.FullName,
+                BirthDate = entity.BirthDate,
+                Position = entity.Position,
+                Salary = entity.Salary,
+                AddressId = entity.AddressId,
+                Phone = entity.Phone,
+            };
+            var result = await _repository.UpdateAsync(employee);
+            return new EmployeeDto
+            {
+                Id = result.Id,
+                FullName = result.FullName,
+                BirthDate = result.BirthDate,
+                Position = result.Position,
+                Salary = result.Salary,
+                AddressId = result.AddressId,
+                Phone = result.Phone,
+            };
         }
     }
 }
--- Library.BLL/Services/OrderServices.cs
+++ Library.BLL/Services/OrderServices.cs
@@ -1,4 +1,5 @@
 ﻿using Library.BLL.interfaces;
+using Library.DLL.Interfaces;
 using Library.Domain.DataTransferObject;
 using Library.Domain.Entities;
 
@@ -6,29 +7,111 @@ namespace Library.BLL.Services
 {
     public class OrderServices : IOrderServices
     {
-        public Task<OrderDto> CreateAsync(OrderDto entity)
+        private readonly IClientRepository _repository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        public OrderServices(IClientRepository repository, IOrderRepository orderRepository, IEmployeeRepository employeeRepository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+            _orderRepository = orderRepository;
+            _employeeRepository = employeeRepository;
+        }
+        public async Task<OrderDto> CreateAsync(OrderDto entity)
+        {
+            var orders = new Order
+            {
+                Id = entity.Id,
+                DateOfIssue = entity.DateOfIssue,
+                ClientId = entity.ClientId,
+                EmployeeId = entity.EmployeeId,
+            };
+            var result = await _repository.CreateAsync(orders);
+            return new OrderDto
+            {
+                Id = result.Id,
+                DateOfIssue = result.DateOfIssue,
+                ClientId = result.ClientId,
+                EmployeeId = result.EmployeeId,
+            };
         }
 
-        public Task<OrderDto> DeleteAsync(int id)
+        public async Task<OrderDto> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _repository.DeleteAsync(id);
+            return new OrderDto
+            {
+                Id = result.Id,
+                DateOfIssue = result.DateOfIssue,
+                ClientId = result.ClientId,
+                EmployeeId = result.EmployeeId,
+            };
         }
 
-        public Task<IEnumerable<OrderDto>> GetAsync()
+        public async Task<IEnumerable<OrderDto>> GetAsync()
         {
-            throw new NotImplementedException();
+            var results = await _repository.GetAsync();
+            return (from result in results
+                    select
+                   new OrderDto
+                   {
+                       Id = result.Id,
+                       DateOfIssue = result.DateOfIssue,
+                       ClientId = result.ClientId,
+                       EmployeeId = result.EmployeeId,
+                   }).ToList();
         }
 
-        public Task<OrderDto> GetAsync(int id)
+        public async Task<OrderDto> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _repository.GetAsync(id);
+            return new OrderDto
+            {
+                Id = result.Id,
+                DateOfIssue = result.DateOfIssue,
+                ClientId = result.ClientId,
+                EmployeeId = result.EmployeeId,
+                Employee = (from results in result.Employee
+                         select
+                         new EmployeeDto
+                         {
+                             Id = results.Id,
+                             FullName = results.FullName,
+                             BirthDate = results.BirthDate,
+                             Position = results.Position,
+                             Salary = results.Salary,
+                             AddressId = results.AddressId,
+                             Phone = results.Phone,
+                         }).ToList(),
+                Client = (from results in result.Client
+                           select
+                           new ClientDto
+                           {
+                               Id = results.Id,
+                               FullName = results.FullName,
+                               BirthDate = results.BirthDate,
+                               AddressId = results.AddressId,
+                               Phone = results.Phone,
+                           }).ToList(),
+            };
         }
 
-        public Task<OrderDto> UpdateAsync(OrderDto entity)
+        public async Task<OrderDto> UpdateAsync(OrderDto entity)
         {
-            throw new NotImplementedException();
+            var order = new Order
+            {
+                Id = entity.Id,
+                DateOfIssue = entity.DateOfIssue,
+                ClientId = entity.ClientId,
+                EmployeeId = entity.EmployeeId,
+            };
+            var result = await _repository.UpdateAsync(order);
+            return new OrderDto
+            {
+                Id = result.Id,
+                DateOfIssue = result.DateOfIssue,
+                ClientId = result.ClientId,
+                EmployeeId = result.EmployeeId,
+            };
         }
     }
 }
